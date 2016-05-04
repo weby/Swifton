561f761d9189cb16d137167926c9da55193fd9c6
Donatas Stundys <donatas.stundys@necolt.com>
2016-05-04 21:13:24 +0300

    Use weby/Stencil

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Swifton",
     dependencies: [
-        .Package(url: "https://github.com/necolt/Stencil.git", versions: Version(0,5,4)..<Version(1,0,0)),
+        .Package(url: "https://github.com/weby/Stencil.git", versions: Version(0,5,4)..<Version(1,0,0)),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Router.git", versions: Version(0,5,0)..<Version(1,0,0))
